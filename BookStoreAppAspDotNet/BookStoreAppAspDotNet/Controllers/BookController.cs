1e2316f660458ace3bb6acdd25b86943c4a33b7e
saqib <saqib@DEV15>
2022-11-01 18:24:43 +0500

    using repository and model folder

---
+++ BookStoreAppAspDotNet/BookStoreAppAspDotNet/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using BookStoreAppAspDotNet.Models;
+using BookStoreAppAspDotNet.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreAppAspDotNet.Controllers
+{
+    public class BookController : Controller    
+    {
+
+        private readonly BookRespository _bookRespository = null;
+
+        public BookController()
+        {
+            _bookRespository = new BookRespository();
+        }
+
+
+
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRespository.GetAllBooks();
+        }
+        public BookModel GetBook(int id )
+        {
+            return _bookRespository.GetBookId(id);
+        }
+        public List<BookModel> SearchBook(string BookName, string AuthorName)
+        {
+            return _bookRespository.SearchBook(BookName, AuthorName);
+        }
+    }
+}
---
+++ BookStoreAppAspDotNet/BookStoreAppAspDotNet/Repository/BookRespository.cs
@@ -0,0 +1,42 @@
+﻿using BookStoreAppAspDotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreAppAspDotNet.Repository
+{
+    public class BookRespository
+    {
+
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookId(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBook(string title , string authorname)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(authorname)).ToList();
+
+        }
+
+        private List<BookModel> DataSource()
+        {
+
+            return new List<BookModel>()
+            {
+                new BookModel(){Id = 1, Title = "MVC" , Author = "Saqib"},
+                new BookModel() { Id = 2, Title = "JAVA", Author = "USAMA" },
+                new BookModel() { Id = 3, Title = "PYHTON", Author = "ATTA" },
+                new BookModel() { Id = 4, Title = "CSHARP", Author = "SAMI" },
+                new BookModel() { Id = 5, Title = "REACT", Author = "Saqib" }
+            };
+        }
+
+    }
+}
